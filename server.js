ab7f8887dbc17ff9bca8140c179d7dda51b12c73
wassup1552 <kavit.test28@gmail.com>
2026-02-13 12:21:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
